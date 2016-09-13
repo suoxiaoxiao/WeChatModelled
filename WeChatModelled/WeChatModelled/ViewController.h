d12a2ba7a80195c85c1ea6d2175fcfcafb45a797
suoxiaoxiao <suoxiaoxiao>
2016-09-13 16:38:42 +0800

    firstCommit

---
+++ WeChatModelled/WeChatModelled/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChatModelled
+//
+//  Created by 索晓晓 on 16/9/13.
+//  Copyright © 2016年 SXiao.RR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeChatModelled/WeChatModelled/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeChatModelled
+//
+//  Created by 索晓晓 on 16/9/13.
+//  Copyright © 2016年 SXiao.RR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
